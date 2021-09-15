810860296a60157962536850fd34d4cfcef70b9a
sepidehmaghami <sdm@gmail.com>
2021-09-15 13:31:50 +0430

    Fix github pages error

--- src/component/timer/timer.js
+++ src/component/timer/timer.js
@@ -1,4 +1,4 @@
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import './timer.css';
 function Timer (){
     const [second , setSecond] =useState('00');
